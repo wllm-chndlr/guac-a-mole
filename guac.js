a01159cc26115685b052ef654f670e7cbd115293
William Chandler <chndlr.wllm@gmail.com>
2019-08-06 08:35:31 -0500

    initial file setup

---
+++ guac.js
@@ -0,0 +1,7 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+
+function startGame() {
+    console.log('starting!');
+}
