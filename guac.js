a80a83d05ac09af0cb2611c493a8a1a4cacf3a42
William Chandler <chndlr.wllm@gmail.com>
2019-08-14 21:36:39 -0500

    style updates, cleanup

--- guac.js
+++ guac.js
@@ -1,6 +1,7 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+
 let lastHole;
 let timeUp = false;
 let score = 0;
@@ -23,6 +24,7 @@ function randomHole(holes) {
 function peep() {
     const time = randomTime(200, 1000);
     const hole = randomHole(holes);
+
     hole.classList.add('up');
     setTimeout(() => {
         hole.classList.remove('up');
